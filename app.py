f027976be66cb784051f177f9c8ee9ee5bb90bee
Igor Tsarev <iam@tio.so>
2014-11-27 16:37:01 +0300

    datetime parsing

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import exifread
 import sys
+import os
+import argparse
+from datetime import datetime
 
-class Image(object):
-    def __init__(self, filename):
-        self.file = open(filename, 'rb')
-
-    def __del__(self):
-        self.file.close()
-
-    def get_date(self):
-        return exifread.process_file(self.file)['Image DateTime']
+def get_date(input_file):
+    try:
+        date = str(exifread.process_file(input_file)['Image DateTime'])
+        date = datetime.strptime(date, "%Y:%m:%d %H:%M:%S")
+    except KeyError:
+        date = datetime.fromtimestamp(os.path.getctime(input_file.name))
+    return date.strftime("%Y-%m-%d")
 
 if __name__ == "__main__":
-    img = Image(sys.argv[1])
-    print img.get_date()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', help="JPEG image file", type=argparse.FileType('rb'), required=True)
+    args = parser.parse_args()
+    print get_date(args.f)
