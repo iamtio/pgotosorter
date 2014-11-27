8822b929f06df6441f054d64511f3a1bfa7700f4
Igor Tsarev <iam@tio.so>
2014-11-27 16:44:35 +0300

    PEP-8

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import argparse
 from datetime import datetime
 
+
 def get_date(input_file):
     try:
         date = str(exifread.process_file(input_file)['Image DateTime'])
@@ -14,6 +15,9 @@ def get_date(input_file):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', help="JPEG image file", type=argparse.FileType('rb'), required=True)
+    parser.add_argument(name='-f',
+                        help="JPEG image file",
+                        type=argparse.FileType('rb'),
+                        required=True)
     args = parser.parse_args()
     print get_date(args.f)
